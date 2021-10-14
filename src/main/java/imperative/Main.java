acba9a725d5812c8e00f9ac8c9da453d77b89546
shaiqhuysenzade <saiqhuseynzade98@gmail.com>
2021-10-14 12:40:58 +0400

    --feat:add new project--

---
+++ src/main/java/imperative/Main.java
@@ -0,0 +1,60 @@
+package imperative;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static imperative.Main.Gender.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+                new Person("Shaiq", MALE),
+                new Person("Zeyneb", FEMALE),
+                new Person("Vagif", MALE),
+                new Person("Narmina", FEMALE)
+        );
+        System.out.println("-----------------Imperative approach-----------------");
+        //Imperative approach
+        List<Person> females = new ArrayList<>();
+        for (Person person : people) {
+            if (FEMALE.equals(person.gender)) {
+                females.add(person);
+            }
+        }
+
+        for (Person female : females) {
+            System.out.println(female);
+        }
+
+        System.out.println("-----------------Declarative approach-----------------");
+        // Declarative approach
+        people.stream()
+                .filter(person -> FEMALE.equals(person.gender))
+                .forEach(System.out::println);
+
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE
+    }
+}
